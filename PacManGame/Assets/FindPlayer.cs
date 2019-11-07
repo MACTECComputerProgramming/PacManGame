cec570b1de301954a4749d7b172351ddab5e83cd
MuchOofs <mdeancline@gmail.com>
2019-11-07 09:01:14 -0600

    Added enemy pathfinding
    The enemies will find the best path to the player while avoiding obstacles.

---
+++ PacManGame/Assets/FindPlayer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class FindPlayer : MonoBehaviour
+{
+    public Transform player;
+    NavMeshAgent agent;
+    Animator animator;
+
+    void Start()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        agent.SetDestination(player.position);
+
+        if (Vector3.Distance(transform.position, player.position) <= 4)
+        {
+            if (!animator.GetCurrentAnimatorStateInfo(0).IsName("attack_03"))
+            {
+                animator.SetBool("attack_03", true);
+            }
+        }
+        else
+        {
+            if (!animator.GetCurrentAnimatorStateInfo(0).IsName("run"))
+            {
+                animator.SetBool("run", true);
+            }
+        }
+    }
+}
