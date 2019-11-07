33f0d3b9357cc941f8704e07c15b61b276b7519d
sp00ker <43855569+sp00ker@users.noreply.github.com>
2019-11-07 08:34:23 -0600

    Add files via upload

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button startGameButton;
+    public string startGameSceneName;
+
+    public void Awake()
+    {
+        startGameButton.onClick.AddListener(StartGame);
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(startGameSceneName);
+    }
+
+   
+}
